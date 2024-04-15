1f166d11a46489434cee930bcf77326319267397
AngeloFilaseta <filaseta.angelo@gmail.com>
2024-04-15 17:04:38 +0200

    feat(monitor): useEffect working as expected

--- alchemist-monitor/src/jsMain/kotlin/it/unibo/alchemist/App.kt
+++ alchemist-monitor/src/jsMain/kotlin/it/unibo/alchemist/App.kt
@@ -21,7 +21,6 @@ import react.Props
 import react.create
 import react.dom.client.createRoot
 import react.dom.html.ReactHTML.p
-import react.useEffect
 import react.useEffectOnce
 import react.useState
 import web.dom.document as webDomDocument
@@ -34,24 +33,28 @@ fun main() {
 }
 
 private val App = FC<Props> {
+    val (list, setList) = useState(emptyList<Pair<Long, Double>?>())
     val subscription by useState(NodesSubscription())
 
     useEffectOnce {
         store.dispatch(AddSubscripionClient("localhost", 1313))
-    }
-    useEffect(listOf(subscription)) {
         sub {
             store.state.subscriptionManager.subscribeAndCollect(
                 subscription,
                 ApolloResponseMapper.nodeSubscriptionMapper(),
                 { pair ->
-                    console.log(pair)
+                    setList {
+                        it + pair
+                    }
                 },
             )
         }
     }
     p {
         +subscription.name()
+    }
+    p {
+        +list.toString()
     }
 }
 
