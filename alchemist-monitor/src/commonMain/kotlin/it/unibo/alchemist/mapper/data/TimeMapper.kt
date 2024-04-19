0d6449fa0aa6877ad4ecc0f82a6a9a8f721b0d3d
AngeloFilaseta <filaseta.angelo@gmail.com>
2024-04-19 12:00:04 +0200

    feat(monitor): add the TimeMapper

---
+++ alchemist-monitor/src/commonMain/kotlin/it/unibo/alchemist/mapper/data/TimeMapper.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2010-2024, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
+package it.unibo.alchemist.mapper.data
+
+import it.unibo.alchemist.boundary.graphql.client.NodesSubscription
+import kotlinx.datetime.Clock
+
+/**
+ * Map the reception of data to the current time.
+ */
+class TimeMapper : DataMapper<NodesSubscription.Data, Long> {
+    override val outputName: String = "time"
+    override fun invoke(data: NodesSubscription.Data?): Long = Clock.System.now().toEpochMilliseconds()
+}
