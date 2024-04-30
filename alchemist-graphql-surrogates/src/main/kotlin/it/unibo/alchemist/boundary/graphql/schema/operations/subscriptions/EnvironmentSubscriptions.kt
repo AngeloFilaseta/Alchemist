de406f05660be7367958c9aa7269db59c42a8132
AngeloFilaseta <filaseta.angelo@gmail.com>
2024-04-30 10:21:49 +0200

    feat(graphql): add simulationTime API to subscriptions

--- alchemist-graphql-surrogates/src/main/kotlin/it/unibo/alchemist/boundary/graphql/schema/operations/subscriptions/EnvironmentSubscriptions.kt
+++ alchemist-graphql-surrogates/src/main/kotlin/it/unibo/alchemist/boundary/graphql/schema/operations/subscriptions/EnvironmentSubscriptions.kt
@@ -16,6 +16,7 @@ import it.unibo.alchemist.model.Environment
 import it.unibo.alchemist.model.Position
 import it.unibo.alchemist.model.util.Environments.subscriptionMonitor
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 /**
  * Exposes alchemist [it.unibo.alchemist.model.Environment] as a GraphQL subscription
@@ -31,4 +32,20 @@ class EnvironmentSubscriptions<T, P : Position<out P>>(environment: Environment<
      */
     @GraphQLDescription("The simulation's environment")
     fun environment(): Flow<EnvironmentSurrogate<T, P>> = environmentMonitor.eventFlow
+
+    /**
+     * The time of the simulation.
+     */
+    @GraphQLDescription("The status of the simulation")
+    fun simulationStatus(): Flow<String> = environmentMonitor.eventFlow.map { env ->
+        env.origin.simulation.status.toString()
+    }
+
+    /**
+     * The time of the simulation.
+     */
+    @GraphQLDescription("The time of the simulation")
+    fun simulationTime(): Flow<Double> = environmentMonitor.eventFlow.map { env ->
+        env.origin.simulation.time.toDouble()
+    }
 }
