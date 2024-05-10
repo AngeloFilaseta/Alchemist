830e309b327da9d4ccddb813566c2f3ba34fdca1
AngeloFilaseta <filaseta.angelo@gmail.com>
2024-05-10 17:44:46 +0200

    chore(monitor): removed almost all state

--- alchemist-monitor/src/jsMain/kotlin/it/unibo/alchemist/state/State.kt
+++ alchemist-monitor/src/jsMain/kotlin/it/unibo/alchemist/state/State.kt
@@ -9,27 +9,17 @@
 
 package it.unibo.alchemist.state
 
-import com.apollographql.apollo3.api.Query
 import com.apollographql.apollo3.api.Subscription
 import it.unibo.alchemist.boundary.graphql.client.GraphQLClient
 import it.unibo.alchemist.dataframe.DataFrame
-import it.unibo.alchemist.mapper.data.DataMapper
 import it.unibo.alchemist.state.actions.DataFramesAction
-import it.unibo.alchemist.state.actions.MapperAction
 import it.unibo.alchemist.state.reducers.dataFramesReducer
-import it.unibo.alchemist.state.reducers.dataMapperReducer
 
 /**
  * State of the monitor component.
  * @param dataframes the dataframes associated with each [Subscription] to each [GraphQLClient].
- * @param currentSubscription the current subscription.
- * @param currentQuery the current query.
- * @param mappers the list of [DataMapper]s.
  */
 data class State(
-    val currentSubscription: Subscription<*>? = null,
-    val currentQuery: Query<*>? = null,
-    val mappers: List<DataMapper<Double>> = listOf(),
     val dataframes: Map<GraphQLClient, DataFrame> = emptyMap(),
 )
 
@@ -39,9 +29,6 @@ data class State(
  * @param action the action to be applied.
  */
 fun rootReducer(state: State, action: Any): State = when (action) {
-    is MapperAction -> state.copy(
-        mappers = dataMapperReducer(state.mappers, action),
-    )
     is DataFramesAction -> state.copy(
         dataframes = dataFramesReducer(state.dataframes, action),
     )
