4765279f4e1519e38a954cc4c36bfe810a2e4c97
AngeloFilaseta <filaseta.angelo@gmail.com>
2024-05-28 12:02:13 +0200

    chore(dsrt): improve plots for response size

--- DSRT-python/plot_nodes_size.py
+++ DSRT-python/plot_nodes_size.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import seaborn as sns
-import seaborn.objects as so
-import numpy as np
 import os
 import matplotlib.pyplot as plt
 
@@ -9,8 +7,9 @@ import matplotlib.pyplot as plt
 sns.set_theme()
 
 # Load the CSV file
-directory = "RESULTS"
+directory = "responseSizeResults"
 
+dataframes = []
 
 for filename in os.listdir(directory):
 
@@ -28,31 +27,36 @@ for filename in os.listdir(directory):
         var_name='Type',
         value_name='Size'
     )
+    melted_df = melted_df[melted_df["Time"] < 175]
+    melted_df["Time"] = melted_df["Time"].round(0)
     melted_df['Size (kB)'] = melted_df['Size'] / 1000
-    # Assign plots
-    limited_df = melted_df.query('Type == "Limited"')
-    full_df = melted_df.query('Type == "Full"')
-    full_size_plot = sns.lineplot(data=full_df, x="Time", y="Size (kB)", color="#B8DE29")
-    limited_size_plot = sns.lineplot(data=limited_df, x="Time", y="Size (kB)", color="#482677")
-    ax2 = full_size_plot.twinx()
-    nodes_plot = sns.lineplot(data=limited_df, x="Time", y="Nodes", color="#000000", linewidth=2, ax=ax2)
+    dataframes.append(melted_df)
+
+data = pd.concat(dataframes, ignore_index=True)
+
+# Assign plots
+limited_df = data.query('Type == "Limited"')
+full_df = data.query('Type == "Full"')
+full_size_plot = sns.lineplot(data=full_df, x="Time", y="Size (kB)", color="#B8DE29")
+limited_size_plot = sns.lineplot(data=limited_df, x="Time", y="Size (kB)", color="#482677")
+ax2 = full_size_plot.twinx()
+nodes_plot = sns.lineplot(data=limited_df, x="Time", y="Nodes", color="#000000", linewidth=1, ax=ax2)
 
 
-    # ✨ Aesthetic ✨
+# ✨ Aesthetic ✨
 
-    ax2.set(ylabel='Size (kB)')
-    nodes_plot.set_ylabel('Number of nodes')
-    nodes_plot.set_ylim(0, df["Nodes"].max() + 10)
-    # LEGEND
-    custom_lines = [
-        plt.Line2D([0], [0], color="#000000", lw=3),
-        plt.Line2D([0], [0], color="#482677", lw=3),
-        plt.Line2D([0], [0], color="#B8DE29", lw=3)
-    ]
-    ax2.legend(custom_lines, ["Number of nodes", "Specific response size", "Full response size"], loc="upper left")
+ax2.set(ylabel='Size (kB)')
+nodes_plot.set_ylabel('Number of nodes')
+nodes_plot.set_ylim(0, data["Nodes"].max() + 10)
+# LEGEND
+custom_lines = [
+    plt.Line2D([0], [0], color="#000000", lw=3),
+    plt.Line2D([0], [0], color="#482677", lw=3),
+    plt.Line2D([0], [0], color="#B8DE29", lw=3)
+]
+ax2.legend(custom_lines, ["Number of nodes", "Specific response size", "Full response size"], loc="upper left")
 
-    plt.title('Influence of Node quantity on response size ', fontsize=12)
-    plt.savefig(filename + ".pdf")
-    plt.clf()
-    # limited_size_plot.set_yticks(np.linspace(0, 2, num=4))
-    # nodes_plot.set_yticks(np.linspace(0, 30, num=2))
+plt.title('Influence of Node quantity on response size ', fontsize=12)
+plt.savefig("responseSize.pdf")
+plt.show()
+plt.clf()
